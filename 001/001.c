7c01a473566ad81cc550de6bec14fe8d063e644d
insomnia-soft <insomnia.soft.git@gmail.com>
2015-01-09 01:32:28 +0100

    problem 1

---
+++ 001/001.c
@@ -0,0 +1,25 @@
+/*
+
+	http://projecteuler.net/index.php?section=problems
+
+	1: Add all the natural numbers below one thousand that are multiples of 3 or 5.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int res = 0, i;
+
+	for (i = 1; i < 1000; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			res += i;
+		}
+	}
+
+	printf("%d\n", res);
+
+	system("PAUSE");
+	return 0;
+}
