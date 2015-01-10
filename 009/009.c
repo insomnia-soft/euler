157f0ff8b59215b90e68e1e75e590255e9d06208
insomnia-soft <insomnia.soft.git@gmail.com>
2015-01-10 03:42:05 +0100

    update

--- 009/009.c
+++ 009/009.c
@@ -1,20 +1,34 @@
+/*
+	projecteuler.net
+	
+	Special Pythagorean triplet
+	Problem 9
+
+	A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+	a^2 + b^2 = c^2
+
+	For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+	Find the product abc.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int i, j, k;
 	
-	
-	for (i = 1; i <= 1000; i++)
-	    for (j = 1; j < 1000; j++)
-	    {
+	for (i = 1; i <= 1000; i++) {
+	    for (j = 1; j < 1000; j++) {
 			k = sqrt(i * i + j * j);
 			
-			if (k * k - i * i == j * j)
-			    if (i + j + k == 1000)
-			        printf("%d + %d + %d = %d\t\t%d * %d * %d = %d\n", i, j, k, i + j + k, i, j, k, i * j *k);
+			if (k * k - i * i == j * j && i + j + k == 1000) {
+				printf("%d + %d + %d = %d\t\t%d * %d * %d = %d\n", i, j, k, i + j + k, i, j, k, i * j *k);
+			}
 		}
+	}
 	
 	system("PAUSE");
 	return 0;
--- 010/010.c
+++ 010/010.c
@@ -1,48 +1,55 @@
+/*
+	projecteuler.net
+
+	Summation of primes
+	Problem 10
+
+	The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+	Find the sum of all the primes below two million.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int i;
 	int n = 2;
-	int djeljiv = 0;
+	int div = 0;
 	
 	int primes[200000] = {0};
 	int primes_count = 0;
 	
-	long long zbroj = 0;
+	long long sum = 0;
 	
-	while (1)
-	{
-		djeljiv = 0;
+	while (1) {
+		div = 0;
 		
-		for (i = 1; i < primes_count; i++)
-		{
-			if (n % primes[i] == 0)
-			{
-				djeljiv = 1;
+		for (i = 1; i < primes_count; i++) {
+			if (n % primes[i] == 0) {
+				div = 1;
 				break;
 			}
-			
 		}
 		
-		if (djeljiv == 0)
-		{
+		if (div == 0) {
 			primes_count++;
 			primes[primes_count] = n;
-			zbroj += n;
+			sum += n;
 		}
 
-		if (n % 10000 == 0)
+		if (n % 10000 == 0) {
 			printf("%d\n", n);
+		}
 
-		if (n >= 2000000)
+		if (n >= 2000000) {
 			break;
+		}
 
 		n++;
 	}
 	
-	printf("sum of all the primes below two million = %lld\n", zbroj);
+	printf("sum of all the primes below two million = %lld\n", sum);
 	
 	system("PAUSE");
 	return 0;
