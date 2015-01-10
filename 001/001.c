50189457f0354a7187c7a747610093045c8b0927
insomnia-soft <insomnia.soft.git@gmail.com>
2015-01-10 04:02:45 +0100

    update

--- 001/001.c
+++ 001/001.c
@@ -1,5 +1,4 @@
 /*
-
 	projecteuler.net
 
 	1: Add all the natural numbers below one thousand that are multiples of 3 or 5.
@@ -9,16 +8,16 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int res = 0, i;
+    int res = 0, i;
 
-	for (i = 1; i < 1000; i++) {
-		if (i % 3 == 0 || i % 5 == 0) {
-			res += i;
-		}
-	}
+    for (i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            res += i;
+        }
+    }
 
-	printf("%d\n", res);
+    printf("%d\n", res);
 
-	system("PAUSE");
-	return 0;
+    system("PAUSE");
+    return 0;
 }
--- 003/003.c
+++ 003/003.c
@@ -1,5 +1,4 @@
 /*
-
 	projecteuler.net
 	
 	Largest prime factor
@@ -8,7 +7,6 @@
 	The prime factors of 13195 are 5, 7, 13 and 29.
 
 	What is the largest prime factor of the number 600851475143 ?
-
 */
 
 #include <stdio.h>
